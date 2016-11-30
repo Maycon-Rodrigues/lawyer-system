30c9589465002aa281b772666b8dd37c5fce6edf
Maycon Rodrigues <mrtdb14@hotmail.com>
2016-11-30 17:27:24 +0000

    Update basic structure

--- app/controllers/pprocesses_controller.rb
+++ app/controllers/pprocesses_controller.rb
@@ -1,5 +1,6 @@
 class PprocessesController < ApplicationController
   before_action :set_pprocess, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
 
   # GET /pprocesses
   # GET /pprocesses.json
